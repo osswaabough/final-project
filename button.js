23a58ff5be51077226a9be288dc789eedd8748fa
osswaabough <Osswanoighanmi@gmail.com>
2024-07-24 13:02:38 +0200

    first commit

---
+++ button.js
@@ -0,0 +1,12 @@
+
+function lightmode() {
+    let element = document.body;
+    element.classList.toggle("light-mode");
+    
+}
+
+function darkmode() {
+    let element = document.body;
+    element.classList.toggle("dark-mode");
+
+}    
